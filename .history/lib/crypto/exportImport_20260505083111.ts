879b4343c20c226336bd2168ed76615b59e7fb36
Mcjohnsontech <mcj26017@gmail.com>
2026-05-05 17:24:27 +0100

    removed history folder

--- .history/lib/crypto/exportImport_20260505083111.ts
+++
@@ -1,9 +0,0 @@
-export async function exportPublicKey(key: CryptoKey) {
-  const exported = await crypto.subtle.exportKey("spki", key);
-  return bufferToBase64(exported);
-}
-
-export async function exportPrivateKey(key: CryptoKey) {
-  const exported = await crypto.subtle.exportKey("pkcs8", key);
-  return bufferToBase64(exported);
-}
